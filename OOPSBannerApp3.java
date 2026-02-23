341a84b71882c4bb371ad6c8cf01957e7cfbf19a
saharajbir <saharajbir63@gmail.com>
2026-02-23 21:47:24 +0530

    Initial commit:add OOPSBannerApp3.java

---
+++ OOPSBannerApp3.java
@@ -0,0 +1,45 @@
+
+
+public class OOPSBannerApp {
+
+    public static void main(String[] args) {
+        // Each line of the banner is created using String.join()
+        System.out.println(String.join(" ", "O", "O", "O", "O", "O"));
+        System.out.println(String.join(" ", "O", " ", " ", " ", "O"));
+        System.out.println(String.join(" ", "O", " ", " ", " ", "O"));
+        System.out.println(String.join(" ", "O", "O", "O", "O", "O"));
+        System.out.println(String.join(" ", "O", " ", " ", " ", " "));
+        System.out.println(String.join(" ", "O", " ", " ", " ", " "));
+        System.out.println(String.join(" ", "O", " ", " ", " ", " "));
+
+        System.out.println(); // spacing between letters
+
+        System.out.println(String.join(" ", "O", "O", "O", "O", "O"));
+        System.out.println(String.join(" ", "O", " ", " ", " ", "O"));
+        System.out.println(String.join(" ", "O", " ", " ", " ", "O"));
+        System.out.println(String.join(" ", "O", "O", "O", "O", "O"));
+        System.out.println(String.join(" ", "O", " ", " ", " ", "O"));
+        System.out.println(String.join(" ", "O", " ", " ", " ", "O"));
+        System.out.println(String.join(" ", "O", "O", "O", "O", "O"));
+
+        System.out.println(); // spacing between letters
+
+        System.out.println(String.join(" ", "P", "P", "P", "P"));
+        System.out.println(String.join(" ", "P", " ", " ", "P"));
+        System.out.println(String.join(" ", "P", "P", "P", "P"));
+        System.out.println(String.join(" ", "P", " ", " ", " "));
+        System.out.println(String.join(" ", "P", " ", " ", " "));
+        System.out.println(String.join(" ", "P", " ", " ", " "));
+        System.out.println(String.join(" ", "P", " ", " ", " "));
+
+        System.out.println(); // spacing between letters
+
+        System.out.println(String.join(" ", "S", "S", "S", "S", "S"));
+        System.out.println(String.join(" ", "S", " ", " ", " ", " "));
+        System.out.println(String.join(" ", "S", " ", " ", " ", " "));
+        System.out.println(String.join(" ", "S", "S", "S", "S", "S"));
+        System.out.println(String.join(" ", " ", " ", " ", " ", "S"));
+        System.out.println(String.join(" ", " ", " ", " ", " ", "S"));
+        System.out.println(String.join(" ", "S", "S", "S", "S", "S"));
+    }
+}
